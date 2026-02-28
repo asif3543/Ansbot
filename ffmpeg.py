98059da682dbf4957cda234f3a285a20818ed23e
asif3543 <ikhanking38@gmail.com>
2026-02-28 22:08:48 +0530

    Delete ffmpeg.py

--- ffmpeg.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def hardsub_video(user_id):
-    os.system(f"""
-    ffmpeg -i /tmp/{user_id}.mp4
-    -vf ass=/tmp/{user_id}.ass
-    -preset ultrafast -crf 30
-    /tmp/{user_id}_out.mp4
-    """)
